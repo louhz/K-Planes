784e3de6aa7dfcea15ade40a12762c989ca372f7
louhz <hlou8@wisc.edu>
2023-02-24 13:00:49 +0800

    debug

--- plenoxels/configs/final/D-NeRF/dnerf_zjuhuman.py
+++ plenoxels/configs/final/D-NeRF/dnerf_zjuhuman.py
@@ -14,7 +14,7 @@ config = {
  'scene_bbox': [[-6, -6, -6], [6, 6, 6]],
 
  # Optimization settings
- 'num_steps': 32001,
+ 'num_steps': 33001,
  'batch_size': 4096,
  'scheduler_type': 'warmup_cosine',
  'optim_type': 'adam',
@@ -31,8 +31,8 @@ config = {
  'time_smoothness_weight_proposal_net': 0.001,
 
  # Training settings
- 'valid_every': 32000,
- 'save_every': 32000,
+ 'valid_every': 33000,
+ 'save_every': 33000,
  'save_outputs': True,
  'train_fp16': True,
 
